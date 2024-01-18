3a09bcca65b4cf09655bbf5f4890091a904db0ad
NimnaKs <nimnakaveesha123456@gmail.com>
2024-01-18 12:47:05 +0530

    Add Filters and configurations

---
+++ src/main/java/lk/ijse/codingchallengejavaee/filter/SecurityFilter.java
@@ -0,0 +1,17 @@
+package lk.ijse.codingchallengejavaee.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class SecurityFilter extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        
+    }
+}
